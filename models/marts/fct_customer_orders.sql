ce8ed538d1b44bf4531633803ccc6d075fba70e0
Sean Seah <seanevanseah@gmail.com>
2024-09-02 15:46:00 +0000

    Simplify with window functions

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -49,17 +49,6 @@ paid_orders as (
     left join customers on orders.user_id = customers.id
 ),
 
-customer_orders as (
-    select 
-        customers.id as customer_id,
-        min(orders.order_date) as first_order_date, 
-        max(orders.order_date) as most_recent_order_date, 
-        count(orders.id) AS number_of_orders
-    from customers
-    left join orders on orders.user_id = customers.id 
-    group by 1
-),
-
 -- Final CTE
 
 final as (
@@ -80,8 +69,13 @@ final as (
         row_number() over (partition by paid_orders.customer_id order by paid_orders.order_id) as customer_sales_seq,
 
         -- new vs returning customer
-        case when customer_orders.first_order_date = paid_orders.order_placed_at
-        then 'new'
+        case 
+            when (
+            rank() over (
+                partition by customer_id
+                order by order_placed_at, order_id
+                ) = 1
+            ) then 'new'
         else 'return' end as nvsr,
 
         --customer lifetime value
@@ -90,12 +84,15 @@ final as (
             order by paid_orders.order_placed_at
         ) as customer_lifetime_value,
 
-        customer_orders.first_order_date as fdos
+        -- first day of sale
+        first_value(paid_orders.order_placed_at) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+            ) as fdos
     from paid_orders
-    left join customer_orders on paid_orders.customer_id = customer_orders.customer_id
-    order by order_id
 )
 
 -- Simple Select Statment
 
 select * from final
+order by order_id
